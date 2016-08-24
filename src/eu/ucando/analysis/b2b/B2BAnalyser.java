7224fee33d9932784202c3ea626077928dd6bb3e
Vitalii Pashinin <vv22161@mail.ru>
2016-08-24 13:09:40 +0200

    b2borders work

--- src/eu/ucando/analysis/b2b/B2BAnalyser.java
+++ src/eu/ucando/analysis/b2b/B2BAnalyser.java
@@ -27,15 +27,17 @@ public class B2BAnalyser {
 	
 	/*
 	 * /home/vpashinin/b2bsales.csv
+	 * 
+	 * order.get("code");
 	 */
 	
 	public static void main(String[] args) throws Exception {
 
 		    MongoClient mongoClient = new MongoClient( "localhost" , 27017 );        
 	        BufferedWriter b2bOrdersWriter = new BufferedWriter(new FileWriter("/home/vpashinin/b2borders.csv"));
-	        	        				 			    	         	    
-				     	         	               	                  	         
-	         DB db = mongoClient.getDB( "som" );
+	        BufferedWriter b2bSalesWriter = new BufferedWriter(new FileWriter("/home/vpashinin/b2bsales.csv"));	         	 			 			    	         	    
+				     	         	               	                  	          
+	         DB db = mongoClient.getDB( "som" ); 
 	         System.out.println("Connect to database successfully");					           
 				
 	         DBCollection coll = db.getCollection("order");
@@ -43,15 +45,18 @@ public class B2BAnalyser {
 
    		     b2bOrdersWriter.write("vatId;customer name;company name");
    		     b2bOrdersWriter.newLine();
+   		     
+   		     b2bSalesWriter.write("Order; Order line item; Month; Day; NIP");
+             b2bSalesWriter.newLine(); 
 	         
    		     /*
-   		      * deliveryAddress.name
+   		      * deliveryAddress.name 
    		      */
    		     DBCursor cursor = coll.find();
 	         
 	         while (cursor.hasNext()) { 
 	        	 DBObject order = cursor.next();
-	        	 DBObject deliveryAddress = (DBObject)order.get("deliveryAddress");
+	        	 DBObject deliveryAddress = (DBObject)order.get("deliveryAddress");	        	 
 	        	
 	        	 String vatId = (String)order.get("vatId");	        	 
 	        	 if (vatId != null){	        
@@ -70,24 +75,58 @@ public class B2BAnalyser {
 				      if (companyname != null){ 
 		        		 b2bOrdersWriter.write(companyname);
 				      }
-				      b2bOrdersWriter.write(";");
+				      b2bOrdersWriter.write(";");	
+				      b2bOrdersWriter.newLine();				      
 				      
 				      
-				      b2bOrdersWriter.newLine(); 
+				      String code = (String)order.get("code");	         	  
+				      if (code != null){ 
+		        		 b2bSalesWriter.write(code);
+				         System.out.println(code);
+				      }
+				      b2bSalesWriter.write(";");
+//				      b2bSalesWriter.newLine();
+				      
+				      
+				      String Orderlineitem = (String)order.get("line item");	         	  
+				      if (Orderlineitem != null){ 
+		        		 b2bSalesWriter.write(Orderlineitem);
+				         System.out.println(Orderlineitem);
+				      }
+				      b2bSalesWriter.write(";");  				      			      
+				      
+				      
+				      String ordermonth = (String)order.get("month");	         	  
+				      if (ordermonth != null){ 
+		        		 b2bSalesWriter.write(ordermonth);
+				         System.out.println(ordermonth);
+				      }
+				      b2bSalesWriter.write(";"); 
+				      
+				      
+				      String orderday = (String)order.get("day");	         	  
+				      if (orderday != null){ 
+		        		 b2bSalesWriter.write(orderday);
+				         System.out.println(orderday);
+				      }
+				      b2bSalesWriter.write(";");
+				      
+				      String orderNIP = (String)order.get("NIP");	         	  
+				      if (orderNIP != null){ 
+		        		 b2bSalesWriter.write(orderNIP);
+				         System.out.println(orderNIP);
+				      }
+				      
+				      b2bSalesWriter.write(";");
+				      b2bSalesWriter.newLine();				      			      
+				      				      
 	        	 }
-			        		 
-	        		 
-	        		 
-	        		 /*
-	        		  * vatId;customer name
-	        		  */	        		      		  	     	
-	        		   
-	        		  
-	        	  	 
 	        	
 	            }
 	           b2bOrdersWriter.flush();
 	           b2bOrdersWriter.close();
+	           b2bSalesWriter.flush();
+	           b2bSalesWriter.close();
 	           
 	           mongoClient.close();
 
@@ -95,7 +134,7 @@ public class B2BAnalyser {
 	
 	    
 	        }
-	  
+
        
 
 	
